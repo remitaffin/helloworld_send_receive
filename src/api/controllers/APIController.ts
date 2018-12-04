a1c02db6b667abe2b13d1487d0ee80401973d413
Remi Taffin <remitaffin@users.noreply.github.com>
2018-12-04 08:37:55 -0500

    Send test JSON from application

--- src/api/controllers/APIController.ts
+++ src/api/controllers/APIController.ts
@@ -8,7 +8,11 @@ export class APIController {
     @Get('/send_to_queue')
     public async send_to_queue(): Promise<any> {
         let successfulCall: boolean;
-        successfulCall = await qm.send_message(env.rabbitmq);
+        const body = {
+            method: 'POST',
+            message: 'http://url/endpoint?test=1',
+        };
+        successfulCall = await qm.send_message(env.rabbitmq, body);
         return {
           success: successfulCall,
         };
